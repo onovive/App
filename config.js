af1d8d8d68644f4845aff1f1ee9ea2628f897d41
digitalcraftsolutions <111793926+digitalcraftsolutions@users.noreply.github.com>
2025-05-26 18:31:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ let currentPasscode = "";
 let uploadedImages = []; // This will store all images for preview
 globalData = {}
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwHVUh1-KgTnCSA9eoEBolDDag1i7yTnPn4xC2u88YmhtHYZ2U8Sk1TbOv7tulkgSzbRA/exec"
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxFeoA3ZfZHCFp9Kn4YHDF5JmeEuGJkkjEe6hysYO_4BzPqSkUfJWRQYMZUMMkpOvDtRA/exec"
