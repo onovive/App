ab2006671c0419134675f44e3cb067176e402c22
onovive <47914783+onovive@users.noreply.github.com>
2025-06-12 13:35:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ let currentPasscode = "";
 let uploadedImages = []; // This will store all images for preview
 globalData = {}
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx7Qz6WZYFQFap8kA6BZZ8cB1reHhmlxSj0WoIckgJOE2GnyZSE7gDNfHM3PQhsMd17PA/exec"
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzlR_dVrKbN3gbgus3wL2FVdR3dhCy5erIISHPyz66lAuAlb-7p8gzAyhzg9uUoMmOqrA/exec"
