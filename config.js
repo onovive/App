20f9fa31e28732056b88a8098c2ee251381c541b
Muhammad Hamza <muhammad.hamza@thepharmacyhub.com>
2026-02-20 05:25:55 +0500

    new version periodiq

--- config.js
+++
@@ -1,7 +0,0 @@
-let clues = [];
-let currentClueIndex = 0;
-let currentPasscode = "";
-let uploadedImages = []; // This will store all images for preview
-globalData = {}
-
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxoajBC_wRRawU4t1noVORmRuWBJGmtlfMNavZX62uoFmiWcv8o0tMsW0RFCuAdYmFbDQ/exec"
